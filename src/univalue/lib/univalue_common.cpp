22ec8eeb3178b0f081230c74291bd8904f3ebe07
odroid <pidath007@gmail.com>
2026-07-15 22:39:10 +1200

    fix: Used a size-compatible index for json_escape

--- src/univalue/lib/univalue_common.cpp
+++ src/univalue/lib/univalue_common.cpp
@@ -327,7 +327,7 @@ std::string json_escape(const std::string& inS)
     std::string outS;
     outS.reserve(inS.size() * 2);
 
-    for (unsigned int i = 0; i < inS.size(); i++) {
+    for (std::string::size_type i = 0; i < inS.size(); i++) {
         unsigned char ch = static_cast<unsigned char>(inS[i]);
         const char *escStr = escapes[ch];
 
