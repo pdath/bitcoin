b62b08710585513bbd643619dec65c094c32fc52
odroid <pidath007@gmail.com>
2026-07-18 12:25:26 +1200

    Removed std::move() from push_backV() as it is already a const object.

--- src/univalue/include/univalue.h
+++ src/univalue/include/univalue.h
@@ -239,7 +239,7 @@ void UniValue::push_backV(It first, It last)
         snapshot.push_back(*it);
     }
     for (const auto& v : snapshot) {
-        push_back(std::move(v));
+        push_back(v);
     }
 #else
     values.insert(values.end(), first, last);
