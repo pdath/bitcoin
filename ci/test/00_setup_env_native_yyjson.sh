5605a74253f527b747f8a70c8159b45bb13ee753
odroid <pidath007@gmail.com>
2026-07-19 19:09:29 +1200

    Improved CI tests.

--- ci/test/00_setup_env_native_yyjson.sh
+++ ci/test/00_setup_env_native_yyjson.sh
@@ -4,10 +4,13 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#!/usr/bin/env bash
+if [ "$(uname -m)" = "aarch64" ]; then
+  export CONTAINER_NAME=ci_native_yyjson_arm64
+else
+  export CONTAINER_NAME=ci_native_yyjson
+fi
 export LC_ALL=C.UTF-8
 export CI_IMAGE_NAME_TAG="mirror.gcr.io/ubuntu:24.04"
-export CONTAINER_NAME=ci_native_yyjson
 export PACKAGES="clang llvm libevent-dev libboost-dev libsqlite3-dev"
 export NO_DEPENDS=1
 export RUN_UNIT_TESTS=true
