42138d11471c68f63e4c670c166f9b78422a4bf3
odroid <pidath007@gmail.com>
2026-07-19 17:15:18 +1200

    Added CI tests.

---
+++ ci/test/00_setup_env_native_yyjson.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2026-present The Bitcoin Knots developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#!/usr/bin/env bash
+export LC_ALL=C.UTF-8
+export CI_IMAGE_NAME_TAG="mirror.gcr.io/ubuntu:24.04"
+export CONTAINER_NAME=ci_native_yyjson
+export PACKAGES="clang llvm libevent-dev libboost-dev libsqlite3-dev"
+export NO_DEPENDS=1
+export RUN_UNIT_TESTS=true
+export RUN_FUNCTIONAL_TESTS=false
+export GOAL="install"
+export BITCOIN_CONFIG="-DWITH_YYJSON=ON -DBUILD_GUI=OFF"
